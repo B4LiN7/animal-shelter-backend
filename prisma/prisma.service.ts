0c79968e785782a907daf4b34dd7b9cb63da24fb
B4LiN7 <balint.k.furedi@outlook.com>
2024-02-20 11:35:34 +0100

    Add warning when can't connect to database on startup.

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -4,6 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log('Connected to the database successfully!');
+    } catch (err) {
+      console.log(`Error connecting to the database: ${err}`);
+    }
   }
 }
