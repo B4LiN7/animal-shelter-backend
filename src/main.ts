1fee6e69530f70fdc1a3bea9357ec94629c9058e
B4LiN7 <balint.k.furedi@outlook.com>
2024-02-21 08:42:52 +0100

    Try to make seed to prisma and fix name change in userservice.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as CookieParser from 'cookie-parser';
+import { main as seedDatabase } from 'prisma/seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,5 +12,6 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(3000);
+  //await seedDatabase();
 }
 bootstrap();
