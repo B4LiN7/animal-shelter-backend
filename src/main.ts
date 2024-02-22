2829552596e8bcc45c37076a3e0ff3030646c069
B4LiN7 <balint.k.furedi@outlook.com>
2024-02-22 12:07:16 +0100

    Remove seedDatabase function call

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as CookieParser from 'cookie-parser';
-import { main as seedDatabase } from 'prisma/seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +11,5 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(3000);
-  //await seedDatabase();
 }
 bootstrap();
