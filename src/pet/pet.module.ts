2e9e3ce8e4594b3fd527e3bf7242bbdfa5569ad2
B4LiN7 <balint.k.furedi@outlook.com>
2024-02-27 12:26:29 +0100

    Add Logger to PetHelperService

--- src/pet/pet.module.ts
+++ src/pet/pet.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { PetService } from './pet.service';
 import { PetController } from './pet.controller';
 import { PrismaModule } from 'prisma/prisma.module';
@@ -9,7 +9,7 @@ import { UserModule } from 'src/user/user.module';
 @Module({
   imports: [PrismaModule, AuthModule, UserModule],
   controllers: [PetController],
-  providers: [PetService, PetHelperService],
+  providers: [PetService, PetHelperService, Logger],
   exports: [PetHelperService],
 })
 export class PetModule {}
